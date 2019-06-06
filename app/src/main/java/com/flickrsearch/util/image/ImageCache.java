f9851aa382db14b5addf5faa87440a8fcf626c9f
t57r <parshenkotaras@gmail.com>
2019-06-06 21:38:55 +0300

    Increased image cache size Focus on EditText when app starts Uploaded apk file

--- app/src/main/java/com/flickrsearch/util/image/ImageCache.java
+++ app/src/main/java/com/flickrsearch/util/image/ImageCache.java
@@ -2,23 +2,14 @@ package com.flickrsearch.util.image;
 
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
-import android.support.v4.graphics.BitmapCompat;
 import android.support.v4.util.LruCache;
 
 class ImageCache {
-    private static final int CACHE_SIZE = 50 * 1024 * 1024; // 50MiB
 
-    private final LruCache<String, Bitmap> bitmapCache = new LruCache<String, Bitmap>(CACHE_SIZE) {
-        protected int sizeOf(@NonNull String key, @NonNull Bitmap value) {
-            return BitmapCompat.getAllocationByteCount(value);
-        }
-    };
+    private static final int ENTRY_COUNT_MAX = 500;
+    private final LruCache<String, Bitmap> bitmapCache = new LruCache<>(ENTRY_COUNT_MAX);
 
-    void put(String url, Bitmap bitmap) {
-        if (url == null || bitmap == null) {
-            throw new NullPointerException("Either url or bitmap is null");
-        }
-
+    void put(@NonNull String url, @NonNull Bitmap bitmap) {
         if (bitmapCache.get(url) == null) {
             bitmapCache.put(url, bitmap);
         }
